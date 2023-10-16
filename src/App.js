718b71cc9945922b3ab14baf8383fe3063303c4c
debasish1452003 <debasishrana1452003@gmail.com>
2023-10-16 22:51:35 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat vel, enim, 
-        atque necessitatibus id qui, quod nesciunt omnis est itaque expedita voluptatum. Consequuntur?
-        ryyrtrtrtrtrtgdghgh
+      <h1> Namaste India
         </h1>
     </div>
   );
