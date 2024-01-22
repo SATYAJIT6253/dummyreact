de133915bb5e4a08bbf01cc2a777ef033f119ace
SATYAJIT6253 <119771949+SATYAJIT6253@users.noreply.github.com>
2024-01-22 11:29:21 +0530

    footer is updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-<<<<<<< HEAD
 import Footer from './components/Footer';
-=======
-import UI from './components/UI';
->>>>>>> 87c933c67f43dd7c2ff02990f10af8d875e1e2bb
 import Navbar from './components/navbar';
 import Homescreen from './screens/Homescreen';
 
@@ -13,8 +9,7 @@ function App() {
   return (
      <div className="Nav">
        <Navbar/>
-       <UI/>
-       <Homescreen/>
+      <Homescreen/>
        <Footer/>
      </div>
   );
